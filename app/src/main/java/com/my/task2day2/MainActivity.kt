7ae676a66eb9d6de625f964c5ced4f5015d8f8e7
unknown <your_email@example.com>
2021-12-03 08:42:28 +0530

    day 2

---
+++ app/src/main/java/com/my/task2day2/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.my.task2day2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputLayout
+
+class MainActivity : AppCompatActivity() {
+    var clickCount =0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val clickCountDisplayView=findViewById<TextView>(R.id.clickCountView)
+        val clickMeButton=findViewById<TextView>(R.id.clickButton)
+        val usernameTextInputView=findViewById<TextInputLayout>(R.id.viewText)
+
+        val mapValue= mutableMapOf("somebody" to 0)
+        var count=0
+
+        clickMeButton.setOnClickListener{
+            var userName=usernameTextInputView.editText?.text.toString()
+            if (userName.isNullOrEmpty())
+            {
+                userName="unknown"
+            }
+            if(!mapValue.containsKey(userName))
+            {
+                mapValue.put(userName,1)
+            }
+            else
+            {
+                count=mapValue[userName]!!
+                count++
+                mapValue[userName]=count
+            }
+            count=mapValue[userName]!!
+            clickCountDisplayView.text="$userName clicked $count times"
+
+        }
+    }
+}
